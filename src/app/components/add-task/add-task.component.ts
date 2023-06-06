715836c723ef515c59107677cfa3f16a71a93da5
Seba Gómez Z <sebamgz@outlook.com>
2023-06-06 00:41:50 -0400

    Se avanza en la clase

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
-  styleUrls: ['./add-task.component.css']
+  styleUrls: ['./add-task.component.css'],
 })
 export class AddTaskComponent {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
@@ -12,25 +14,31 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  subscription?: Subscription;
+  showAddTask?: boolean;
 
-  onSubmit(){
-   if(!this.text){
-    alert('Añade el texto');
-    return;
-   }
-
-   const newTask = {
-    text: this.text,
-    day: this.day,
-    reminder: this.reminder,
-   }
-
-   this.onAddTask.emit(newTask);
-
-   this.text = '';
-   this.day = '';
-   this.reminder = false;
-
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showAddTask = value;
+    });
   }
 
+  onSubmit() {
+    if (!this.text) {
+      alert('Añade el texto');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   title: string = 'Test en Angular';
+  showAddTask?: boolean;
+  subscription?: Subscription;
 
-  toggleAddTask(){
-    console.log("toggle");
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
   }
 
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
 }
